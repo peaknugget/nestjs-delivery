740b937b000e52c794e0eb804a594473f47d0558
braverokmc79 <braverokmc79@gmail.com>
2025-08-23 23:39:08 +0900

    update env

--- backend/apps/payment/src/app.module.ts
+++ backend/apps/payment/src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import * as Joi from 'joi';
 import { PaymentModule } from './payment/payment.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NOTIFICATION_SERVICE } from '@app/common';
 
 @Module({
   imports: [
@@ -22,6 +24,23 @@ import { PaymentModule } from './payment/payment.module';
         synchronize: true,
       }),
       inject: [ConfigService],
+    }),
+
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: NOTIFICATION_SERVICE,
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow<string>('NOTIFICATION_HOST'),
+              port: configService.getOrThrow<number>('NOTIFICATION_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
     }),
 
     PaymentModule,
